20d90d21f493dbedc6855a14b83bd7b7d90bc452
xander557 <yikanwang557@gmail.com>
2021-03-08 01:21:14 -0800

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+[![build status of master](https://travis-ci.org/xander557/Triangle.py.svg?branch=master)](https://travis-ci.org/xander557/Triangle.py)
---
+++ TestTriangle.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Updated Jan 21, 2018
+The primary goal of this file is to demonstrate a simple unittest implementation
+
+@author: jrr/ Yikan Wang
+@author: rk
+"""
+
+import unittest
+
+from Triangle import classifyTriangle
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestTriangles(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+
+    def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        
+    def testEquilateralTriangles(self):
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testRightTriangleC(self):
+        self.assertEqual(classifyTriangle(10, 9, 7), 'Scalene')
+
+    def RightTriangleD(self):
+        self.assertEqual(classifyTriangle(2, 2, 3), 'Isosceles')
+
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
+
